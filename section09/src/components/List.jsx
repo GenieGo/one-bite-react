2cf064a4d5cdec356ec7b5b4cb9ff981bf896844
yuoper1 <yuoper1@gmail.com>
2024-03-19 02:46:51 +0900

    9.1) useReducer 사용

---
+++ section09/src/components/List.jsx
@@ -0,0 +1,43 @@
+import "./List.css";
+import TodoItem from "./TodoItem";
+import { useState } from "react";
+const List = ({ todos, onUpdate, onDelete }) => {
+  const [search, setSearch] = useState("");
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+  const getFilteredDate = () => {
+    if (search === "") {
+      return todos;
+    } else {
+      return todos.filter((todo) =>
+        todo.content.toLowerCase().includes(search.toLowerCase())
+      );
+    }
+  };
+  const filteredTodos = getFilteredDate();
+  return (
+    <div className="List">
+      <h4>Todo List ✔</h4>
+      <input
+        type="text"
+        placeholder="검색어를 입력하세요"
+        onChange={onChangeSearch}
+      />
+      <div className="todos-wraper">
+        {filteredTodos.map((todo) => {
+          return (
+            <TodoItem
+              key={todo.id}
+              {...todo}
+              onUpdate={onUpdate}
+              onDelete={onDelete}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default List;
