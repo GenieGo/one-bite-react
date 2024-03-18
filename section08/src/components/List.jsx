e61725665bc600c121462536883ba4aa60a49919
yuoper1 <yuoper1@gmail.com>
2024-03-18 19:29:55 +0900

    8.5) todo list render

--- section08/src/components/List.jsx
+++ section08/src/components/List.jsx
@@ -1,13 +1,33 @@
 import "./List.css";
 import TodoItem from "./TodoItem";
-const List = () => {
+import { useState } from "react";
+const List = ({ todos }) => {
+  const [search, setSearch] = useState("");
+  const onChangeSearch = (e) => {
+    setSearch(e.target.value);
+  };
+  const getFilteredDate = () => {
+    if (search === "") {
+      return todos;
+    } else {
+      return todos.filter((todo) =>
+        todo.content.toLowerCase().includes(search.toLowerCase())
+      );
+    }
+  };
+  const filteredTodos = getFilteredDate();
   return (
     <div className="List">
       <h4>Todo List ✔</h4>
-      <input type="text" placeholder="검색어를 입력하세요" />
+      <input
+        type="text"
+        placeholder="검색어를 입력하세요"
+        onChange={onChangeSearch}
+      />
       <div className="todos-wraper">
-        <TodoItem />
-        <TodoItem />
+        {filteredTodos.map((todo) => {
+          return <TodoItem key={todo.id} {...todo} />;
+        })}
       </div>
     </div>
   );
