a0630766288a974ff075595288b823997afdc32c
yuoper1 <yuoper1@gmail.com>
2024-03-24 18:35:49 +0900

    12.5~6) 페이지 라우팅 동적경로 / 이미지, 폰트, 레이아웃 설정

--- section12/src/pages/Home.jsx
+++ section12/src/pages/Home.jsx
@@ -1,4 +1,9 @@
+import { useSearchParams } from "react-router-dom";
 const Home = () => {
+  const [params, setParams] = useSearchParams();
+  // console.log(params.value)
+  // /?value=11
+
   return <div>Home</div>;
 };
 
